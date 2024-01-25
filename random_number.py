ebcaa41c0456adc6d72b94427e41659fd01e0695
Claudia <cldelac@siue.edu>
2024-01-25 15:05:12 -0600

    change to random_number file

--- random_number.py
+++ random_number.py
@@ -8,4 +8,10 @@ while guess != number:
         print("Too low!")
     else:
         print("Too high!")
+<<<<<<< HEAD
     guess = int(input("Make another guess: "))
+=======
+    guess = int(input("Try again: "))
+>>>>>>> 71dd1d518bb455621ff54fa26ed4067aacff353f
+
+print("Congrats! You guessed the number! It was", number)
