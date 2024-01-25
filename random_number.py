a634f3fee44024f77ba86c1dd80f8cec6154acd7
Claudia <cldelac@siue.edu>
2024-01-25 12:23:17 -0600

    adding random_number file

---
+++ random_number.py
@@ -0,0 +1,13 @@
+import random
+
+number = random.randint(1, 10)
+guess = int(input("Guess a number between 1 and 10: "))
+
+while guess != number:
+    if guess < number:
+        print("Too low!")
+    else:
+        print("Too high!")
+    guess = int(input("Guess again: "))
+
+print("You guessed it! The number was", number)
